b87121cd6b3eae96604093b83cfc05a5c5b2b729
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-11-02 23:46:13 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,16 @@
+    // урок 3-4
+/** @jsxImportSource @emotion/react */
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles/theme";
+import { GlobalStyles } from "./styles/global";
+import { TasksPage } from "./pages/tasks-page";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <TasksPage />
+    </ThemeProvider>
+  );
+};
+export default App;
